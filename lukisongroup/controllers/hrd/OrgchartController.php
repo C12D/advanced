ea70a87c4518b28a14e5ac1e4c736d005f6d560d
Piter Novian <ptr.nov@gmail.com>
2015-10-04 17:20:53 -0700

    orgupdate

--- lukisongroup/controllers/hrd/OrgchartController.php
+++ lukisongroup/controllers/hrd/OrgchartController.php
@@ -1,24 +1,16 @@
 <?php
-/**
- * NOTE: Nama Class harus diawali Hurup Besar
- * Server Linux 	: hurup besar/kecil bermasalah -case sensitif-
- * Server Windows 	: hurup besar/kecil tidak bermasalah
- * Author: -ptr.nov-
-*/
 
 namespace lukisongroup\controllers\hrd;
 
-/* VARIABLE BASE YII2 Author: -YII2- */
 use Yii;
+use lukisongroup\models\hrd\Orgchart;
+use lukisongroup\models\hrd\OrgchartSearch;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
-/* VARIABLE PRIMARY JOIN/SEARCH/FILTER/SORT Author: -ptr.nov- */
-//use app\models\hrd\Dept;			/* TABLE CLASS JOIN */
-//use app\models\hrd\DeptSearch;		/* TABLE CLASS SEARCH */
-	
+
 /**
- * HRD | CONTROLLER EMPLOYE .
+ * Orgchart1Controller implements the CRUD actions for Orgchart model.
  */
 class OrgchartController extends Controller
 {
@@ -26,26 +18,109 @@ class OrgchartController extends Controller
     {
         return [
             'verbs' => [
-                'class' => VerbFilter::className(['dashboard']),
+                'class' => VerbFilter::className(),
                 'actions' => [
-                    //'delete' => ['post'],
-					'save' => ['post'],
+                    'delete' => ['post'],
                 ],
             ],
         ];
     }
 
     /**
-     * ACTION INDEX
+     * Lists all Orgchart models.
+     * @return mixed
      */
     public function actionIndex()
     {
-		/*	variable content View Employe Author: -ptr.nov- */
-       // $searchModel_Dept = new DeptSearch();
-		//$dataProvider_Dept = $searchModel_Dept->search(Yii::$app->request->queryParams);
+		//$this->enableCsrfValidation = false; 
 		
-		return $this->render('index');
+        $searchModel = new OrgchartSearch();
+        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+
+        return $this->render('index', [
+            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
+        ]);
     }
+
+    /**
+     * Displays a single Orgchart model.
+     * @param string $id
+     * @return mixed
+     */
+    public function actionView($id)
+    {
+        return $this->render('view', [
+            'model' => $this->findModel($id),
+        ]);
+    }
+
+    /**
+     * Creates a new Orgchart model.
+     * If creation is successful, the browser will be redirected to the 'view' page.
+     * @return mixed
+     */
+    public function actionCreate()
+    {
+        $model = new Orgchart();
+
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->id]);
+        } else {
+            return $this->render('create', [
+                'model' => $model,
+            ]);
+        }
+    }
+
+    /**
+     * Updates an existing Orgchart model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     * @param string $id
+     * @return mixed
+     */
+    public function actionUpdate($id)
+    {
+        $model = $this->findModel($id);
+
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->id]);
+        } else {
+            return $this->render('update', [
+                'model' => $model,
+            ]);
+        }
+    }
+
+    /**
+     * Deletes an existing Orgchart model.
+     * If deletion is successful, the browser will be redirected to the 'index' page.
+     * @param string $id
+     * @return mixed
+     */
+    public function actionDelete($id)
+    {
+        $this->findModel($id)->delete();
+
+        return $this->redirect(['index']);
+    }
+
+    /**
+     * Finds the Orgchart model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param string $id
+     * @return Orgchart the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($id)
+    {
+        if (($model = Orgchart::findOne($id)) !== null) {
+            return $model;
+        } else {
+            throw new NotFoundHttpException('The requested page does not exist.');
+        }
+    }
+	
 	public function actionChat()
     {
         //$model = new LoginForm();
