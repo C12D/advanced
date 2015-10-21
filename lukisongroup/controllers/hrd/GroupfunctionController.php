3cd8e09603e7d666cd02b9de70dda3d15b4db466
Piter Novian <ptr.nov@gmail.com>
2015-10-21 10:07:17 +0700

    corp

--- lukisongroup/controllers/hrd/GroupfunctionController.php
+++ lukisongroup/controllers/hrd/GroupfunctionController.php
@@ -25,7 +25,29 @@ class GroupfunctionController extends Controller
             ],
         ];
     }
-
+	
+	/* -- Created Session Time Author By ptr.nov --*/
+	public function beforeAction(){
+			if (Yii::$app->user->isGuest)  {
+				 Yii::$app->user->logout();
+                   $this->redirect(array('/site/login'));  //
+			}
+            // Check only when the user is logged in
+            if (!Yii::$app->user->isGuest)  {
+               if (Yii::$app->session['userSessionTimeout']< time() ) {
+                   // timeout
+                   Yii::$app->user->logout();
+                   $this->redirect(array('/site/login'));  //
+               } else {
+                   //Yii::$app->user->setState('userSessionTimeout', time() + Yii::app()->params['sessionTimeoutSeconds']) ;
+				   Yii::$app->session->set('userSessionTimeout', time() + Yii::$app->params['sessionTimeoutSeconds']);
+                   return true; 
+               }
+            } else {
+                return true;
+            }
+    }
+	
     public function actionIndex()
     {
         $searchModel = new GroupfunctionSearch();
--- lukisongroup/models/hrd/Corp.php
+++ lukisongroup/models/hrd/Corp.php
@@ -26,7 +26,7 @@ class Corp extends \yii\db\ActiveRecord
     public static function tableName()
     {
         //return '{{dbm000.a0001}}';
-		return 'dbm002.u0001';
+		return '{{u0001}}';
     }
 
 	/* [3] RULE SCENARIO -> DetailView */
@@ -36,6 +36,9 @@ class Corp extends \yii\db\ActiveRecord
             [['CORP_ID','CORP_NM'], 'required'],
             [['CORP_ID'], 'string', 'max' => 5],
             [['CORP_NM'], 'string', 'max' => 30],
+			[['CORP_DCRP'],'string'],
+			[['CREATED_BY','UPDATED_BY'], 'string', 'max' => 50],
+			[['UPDATED_TIME'],'safe'],
          ];
     }
 
